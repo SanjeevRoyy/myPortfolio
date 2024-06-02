ba43f86af00acdd1a1e2a4a6e9ab22b52c715a9f
SanjeevRoyy <abiralmanausya9@gmail.com>
2024-06-02 14:50:04 +0545

    created portfolio home section

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import Navbar from "./components/Navbar"
+import Home from "./pages/Home"
+
+
+function App() {
+
+
+  return (
+    <>
+<Navbar/>
+<Home/>
+    </>
+  )
+}
+
+export default App
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,42 @@
+import React, { useState } from "react";
+import { RxHamburgerMenu } from "react-icons/rx";
+import { Link } from "react-router-dom";
+
+
+const Navbar = () => {
+  const [showNavbar, setShowNavbar] = useState(false);
+
+  const toggleNavbar = () => {
+    setShowNavbar(!showNavbar);
+  };
+
+  return (
+    <>
+      <div className="hamburger">
+        <RxHamburgerMenu onClick={toggleNavbar} size={30} />
+      </div>
+      <div className={`NavbarDiv ${showNavbar ? "show" : ""}`}>
+        <div>
+            <h3>Sanjeev.dev</h3>
+        </div>
+        <div className="NavLinks">
+          <Link className="link" to="/">
+            Home
+          </Link>
+          <Link className="link" to="/courses">
+            About
+          </Link>
+          <Link className="link" to="/services">
+            Project
+          </Link>
+          <Link className="link" to="/blog">
+            Contact
+          </Link>
+         
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default Navbar;
