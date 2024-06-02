4dc18eae11403c540b2b3e106f5135eeb3c64b41
SanjeevRoyy <abiralmanausya9@gmail.com>
2024-06-02 15:32:53 +0545

    created about page

---
+++ src/pages/About.jsx
@@ -0,0 +1,26 @@
+import React from "react";
+
+const About = () => {
+  return (
+    <div className="AboutDiv">
+      <div>
+        <img src="img/about.jpg" alt="" />
+      </div>
+      <div className="Right">
+        <h4>ABOUT ME</h4>
+        <h2>A dedicated Front-End Developer based in Kathmandu , Nepal</h2>
+        <p>
+          As a junior Front-End Developer, I possess an impressive arsenal of
+          skills in HTML,CSS,javaScript,React,Tailwind and SCSS . I excel in
+          designing and maintaning responsive website that offer smooth user
+          experience. My expertise lies in crafting dynamic,engaging interface through
+          writting clean and optimized code and utilizing cutting-edge development tools and
+          techniques. I am also a team player who thrives in collaborating with cross-functional teams
+          to produce outstanding web applications.
+        </p>
+      </div>
+    </div>
+  );
+};
+
+export default About;
