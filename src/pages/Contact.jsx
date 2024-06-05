d97a7b2aa953fea73b681dcb848202f48dd0fb05
SanjeevRoyy <abiralmanausya9@gmail.com>
2024-06-05 15:30:00 +0545

    added contact icons

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -6,21 +6,27 @@ import { IoMdMail } from "react-icons/io";
 const Contact = () => {
   return (
     <div id="contact" className="ContactDiv">
-      <h3 style={{textAlign:"center"}}>GET IN TOUCH</h3>
+      <h3 style={{ textAlign: "center" }}>GET IN TOUCH</h3>
       <div className="Details">
         <div>
-          <FaLocationDot />
-          <h3>Address</h3>
+          <div className="AddressSection">
+            <h3>Address</h3>
+            <FaLocationDot />
+          </div>
           <p>New Baneshwor, Kathmandu</p>
         </div>
         <div>
-          <FaPhoneAlt />
-          <h3>Phone</h3>
+          <div className="AddressSection">
+            <h3>Phone</h3>
+            <FaPhoneAlt />
+          </div>
           <p>+977-9829310011</p>
         </div>
         <div>
-          <IoMdMail />
-          <h3>Email</h3>
+          <div className="AddressSection">
+            <h3>Email</h3>
+            <IoMdMail />
+          </div>
           <p>abiralmanausya9@gmail.com</p>
         </div>
       </div>
