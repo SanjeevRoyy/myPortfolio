8d7d5d9da93ab5eab954a8e4bb35898c68c80557
SanjeevRoyy <abiralmanausya9@gmail.com>
2024-06-07 12:58:17 +0545

    added nabvar hidden on click

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,36 +1,36 @@
 import React, { useState } from "react";
 import { RxHamburgerMenu } from "react-icons/rx";
-import { Link, Route, Routes, useNavigate } from "react-router-dom";
+import { RxCross1 } from "react-icons/rx";
 
 const Navbar = () => {
   const [showNavbar, setShowNavbar] = useState(false);
-  const navigate = useNavigate();
-
   const toggleNavbar = () => {
     setShowNavbar(!showNavbar);
+  };
+  //make navbar hidden when clicked 
+  const hideNavbar = () => {
+    setShowNavbar(false);
   };
 
   return (
     <>
-      <div className="hamburger">
-        <RxHamburgerMenu onClick={toggleNavbar} size={30} />
+      <div className="hamburger" onClick={toggleNavbar}>
+        {showNavbar ? <RxCross1 size={25} /> : <RxHamburgerMenu size={25} />}
       </div>
       <div className={`NavbarDiv ${showNavbar ? "show" : ""}`}>
-        
-        <div className="NavLinks">
-          <a href="#home"  className="link" >
+        <div className="NavLinks" onClick={hideNavbar}>
+          <a href="#home" className="link">
             Home
           </a>
-          <a href="#about" className="link" >
+          <a href="#about" className="link">
             About
           </a>
-          <a href="#project" className="link" >
+          <a href="#project" className="link">
             Project
           </a>
-          <a href="#contact" className="link" to="/blog">
+          <a href="#contact" className="link">
             Contact
           </a>
-         
         </div>
       </div>
     </>
